b83776d586dadcabae6ae98b45b21efb8f0868a7
Hanulcik <grhanulcik@gmail.com>
2021-12-17 10:55:09 -0500

    essentials pt1

--- src/SqueakyClean.java
+++ src/SqueakyClean.java
@@ -1,25 +1,17 @@
-package com.company;
-
-public class Main {
-
-    public static void main(String[] args) {
-	    String test = "à-ḃç";
-
-        System.out.println(clean(test));
-    }
-
+public class SqueakyClean {
 
     static String clean(String identifier) {
         identifier = identifier.replaceAll("\uD83D\uDE00", "");
         identifier = identifier.replaceAll(" ", "_");
         identifier = identifier.replaceAll("\0", "CTRL");
         if (identifier.contains("-")) {
-            Character ch = identifier.charAt(identifier.indexOf("-" + 1));
+            Character ch = identifier.charAt(identifier.indexOf("-") + 1);
             Character uppercase = Character.toUpperCase(ch);
             String target = "-" + ch;
             identifier = identifier.replaceAll(target, String.valueOf(uppercase));
         }
         identifier = identifier.replaceAll("[α-ω]+", "");
+        identifier = identifier.replaceAll("[0-9]", "");
         return identifier;
     }
 }
